21fe31537fec03f0e691a92e67778af66a5c16aa
Cristian Serrano <34175327+cristianserran0@users.noreply.github.com>
2024-10-27 09:15:14 +0100

    More descriptive Nav behaviour

--- js/header-behaviour.js
+++ js/header-behaviour.js
@@ -1,4 +1,4 @@
-// Hide Header on on scroll down
+// Hide Header on scroll down
 var didScroll;
 var lastScrollTop = 0;
 var delta = 50;
@@ -22,15 +22,15 @@ function hasScrolled() {
     if(Math.abs(lastScrollTop - st) <= delta)
         return;
     
-    // If they scrolled down and are past the navbar, add class .nav-up.
+    // If they scrolled down and are past the navbar, add class .nav-hidden.
     // This is necessary so you never see what is "behind" the navbar.
     if (st > lastScrollTop && st > navBarHeight){
         // Scroll Down
-        $('header').removeClass('nav-down').addClass('nav-up');
+        $('header').removeClass('nav-visible').addClass('nav-up');
     } else {
         // Scroll Up
         if(st + $(window).height() < $(document).height()) {
-            $('header').removeClass('nav-up').addClass('nav-down');
+            $('header').removeClass('nav-hidden').addClass('nav-down');
         }
     }
     
